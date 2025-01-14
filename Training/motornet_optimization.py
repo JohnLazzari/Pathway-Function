b937598dd32e939375e5eb0ec9e69498da45e5a3
John Lazzari <lazzarijohn2@gmail.com>
2025-01-14 16:42:22 -0500

    training reaching with mRNN

--- Training/motornet_optimization.py
+++ Training/motornet_optimization.py
@@ -5,47 +5,36 @@ import numpy as np
 import torch as th
 import matplotlib.pyplot as plt
 import motornet as mn
+from mRNNTorch.mRNN import mRNN
+from mRNNTorch.utils import get_region_activity, get_initial_condition
 
 effector = mn.effector.ReluPointMass24()
 env = mn.environment.RandomTargetReach(effector=effector, max_ep_duration=1.)
 
 class Policy(th.nn.Module):
-    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int, device):
+    def __init__(self, config, hidden_dim, output_dim, device):
         super().__init__()
-        self.device = device
+
+        self.config = config
         self.hidden_dim = hidden_dim
+        self.device = device
         self.n_layers = 1
 
         #parameters for visualizing network activity
         self.activity = np.array([])
         self.count = 0
         
-        self.gru = th.nn.GRU(input_dim, hidden_dim, 1, batch_first=True)
+        self.mrnn = mRNN(config, device=device)
         self.fc = th.nn.Linear(hidden_dim, output_dim)
         self.sigmoid = th.nn.Sigmoid()
 
-        # the default initialization in torch isn't ideal
-        for name, param in self.named_parameters():
-            if name == "gru.weight_ih_l0":
-                th.nn.init.xavier_uniform_(param)
-            elif name == "gru.weight_hh_l0":
-                th.nn.init.orthogonal_(param)
-            elif name == "gru.bias_ih_l0":
-                th.nn.init.zeros_(param)
-            elif name == "gru.bias_hh_l0":
-                th.nn.init.zeros_(param)
-            elif name == "fc.weight":
-                th.nn.init.xavier_uniform_(param)
-            elif name == "fc.bias":
-                th.nn.init.constant_(param, -5.)
-            else:
-                raise ValueError
-        
         self.to(device)
 
-    def forward(self, x, h0):
-        y, h = self.gru(x[:, None, :], h0)
-        u = self.sigmoid(self.fc(y)).squeeze(dim=1)
+    def forward(self, h, obs):
+        x, h = self.mrnn(h, obs[None, :, :])
+        h = h.squeeze(0)
+        m1_act = get_region_activity(self.mrnn, h, "alm")
+        u = self.sigmoid(self.fc(m1_act)).squeeze(dim=1)
         u_ = u.clone().norm()
         self.activity = np.append(self.activity, u_.detach().numpy())
         self.count += 1
@@ -62,47 +51,54 @@ class Policy(th.nn.Module):
     
 device = th.device("cpu")
 
-policy = Policy(env.observation_space.shape[0], 32, env.n_muscles, device=device)
+config = "configurations/mRNN_hyperdirect.json"
+model_save_path = "checkpoints/mRNN_reaching"
+policy = Policy(config, 50, env.n_muscles, device=device)
 optimizer = th.optim.Adam(policy.parameters(), lr=10**-3)
 
-
 batch_size = 1
-n_batch = 6000
+n_batch = 10000
 losses = []
 interval = 250
 
 def l1(x, y):
-  """L1 loss"""
-  return th.mean(th.sum(th.abs(x - y), dim=-1))
+    """L1 loss"""
+    return th.mean(th.sum(th.abs(x - y), dim=-1))
 
 for batch in range(n_batch):
-  # initialize batch
-  h = policy.init_hidden(batch_size=batch_size)
-  obs, info = env.reset(options={"batch_size": batch_size})
-  terminated = False
+    # initialize batch
+    h = th.zeros(size=(batch_size, policy.mrnn.total_num_units))
+    h = get_initial_condition(policy.mrnn, h)
+    obs, info = env.reset(options={"batch_size": batch_size})
+    terminated = False
 
-  # initial positions and targets
-  xy = [info["states"]["fingertip"][:, None, :]]
-  tg = [info["goal"][:, None, :]]
+    # initial positions and targets
+    xy = [info["states"]["fingertip"][:, None, :]]
+    tg = [info["goal"][:, None, :]]
 
-  # simulate whole episode
-  while not terminated:  # will run until `max_ep_duration` is reached
-    action, h = policy(obs, h)
-    obs, reward, terminated, truncated, info = env.step(action=action)
+    # simulate whole episode
+    while not terminated:  # will run until `max_ep_duration` is reached
+        action, h = policy(h, obs)
+        obs, reward, terminated, truncated, info = env.step(action=action)
 
-    xy.append(info["states"]["fingertip"][:, None, :])  # trajectories
-    tg.append(info["goal"][:, None, :])  # targets
+        xy.append(info["states"]["fingertip"][:, None, :])  # trajectories
+        tg.append(info["goal"][:, None, :])  # targets
 
-  # concatenate into a (batch_size, n_timesteps, xy) tensor
-  xy = th.cat(xy, axis=0)
-  tg = th.cat(tg, axis=0)
-  loss = l1(xy, tg)  # L1 loss on position
-  
-  # backward pass & update weights
-  optimizer.zero_grad() 
-  loss.backward()
-  th.nn.utils.clip_grad_norm_(policy.parameters(), max_norm=1.)  # important!
-  optimizer.step()
-  losses.append(loss.item())
-  if (batch % interval == 0) and (batch != 0):
-    print("Batch {}/{} Done, mean policy loss: {}".format(batch, n_batch, sum(losses[-interval:])/interval))
+    # concatenate into a (batch_size, n_timesteps, xy) tensor
+    xy = th.cat(xy, axis=0)
+    tg = th.cat(tg, axis=0)
+    loss = l1(xy, tg)  # L1 loss on position
+    
+    # backward pass & update weights
+    optimizer.zero_grad() 
+    loss.backward()
+    th.nn.utils.clip_grad_norm_(policy.parameters(), max_norm=1.)  # important!
+    optimizer.step()
+    losses.append(loss.item())
+    if (batch % interval == 0) and (batch != 0):
+        print("Batch {}/{} Done, mean policy loss: {}".format(batch, n_batch, sum(losses[-interval:])/interval))
+
+    if batch % 1000 == 0:
+        th.save({
+            'agent_state_dict': policy.state_dict(),
+        }, model_save_path + '.pth')
